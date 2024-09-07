9a7cbf968f3fcc21445f31a63dac22020d17a74b
rrroyal <royal@shameful.xyz>
2024-09-07 20:24:09 +0200

    Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,5 +24,8 @@ let package = Package(
 			name: "NetworkKitTests",
 			dependencies: ["NetworkKit"]
 		)
+	],
+	swiftLanguageModes: [
+		.v6
 	]
 )
